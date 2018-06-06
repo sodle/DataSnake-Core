2533e18251aa22f9900377348b070d75251c5421
Scott Odle <sjodle@asu.edu>
2018-06-06 15:09:10 -0700

    setup script

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='datasnake',
+    version='0.1.0',
+    description='CLI for fast and easy database queries, tailing, pagination, introspection. '
+                'DB engine agnostic. Backbone of Splunk TA-DataSnake.',
+    author='Scott Odle (Arizona State University)',
+    author_email='scott.odle@asu.edu',
+    url='https://github.com/sodle/datasnake-core',
+    license=['GPL v3'],
+    packages=['datasnake'],
+    install_requires=[
+        'six',
+        'pandas',
+        'sqlalchemy',
+        'docopt'
+    ],
+    entry_points={
+        'console_scripts': [
+            'datasnake = datasnake.__main__:_main'
+        ]
+    }
+)
