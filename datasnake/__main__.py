2b9a173bfabc9a0cdc60134165651aaaa2084534
Scott Odle <sjodle@asu.edu>
2018-06-06 16:01:06 -0700

    CLI grammar and help

---
+++ datasnake/__main__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""Query and explore different types of databases quickly and easily.
+
+Usage:
+    datasnake <connection_string> <sql_query> [--output-format=<output_format>]
+    datasnake <connection_string> <sql_query> --index=<index> [--offset=<offset>] [--output-format=<output_format>]
+    datasnake list-tables <connection_string>
+    datasnake (-h | --help)
+    datasnake --version
+
+Options:
+    -h --help                           Show this help.
+    --version                           Show version.
+    --index=<index>                     Column to use as index (for sorting and checkpointing).
+    --offset=<offset>                   Only fetch rows with index value strictly greater than this.
+    --output-format=<output_format>     Output rows in "dbx" (Splunk DBX) or "json" format [default: "dbx"].
+"""
+from docopt import docopt
+
+
+def _main():
+    arguments = docopt(__doc__, version='datasnake 0.1.0')
+
+
+if __name__ == '__main__':
+    _main()
