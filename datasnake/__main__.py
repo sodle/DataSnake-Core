f271f86b6caf64b1e9c7f4a9ba4d03b5747c758e
Scott Odle <sjodle@asu.edu>
2018-06-07 09:35:04 -0700

    shell functions for CLI

--- datasnake/__main__.py
+++ datasnake/__main__.py
@@ -13,13 +13,27 @@ Options:
     --version                           Show version.
     --index=<index>                     Column to use as index (for sorting and checkpointing).
     --offset=<offset>                   Only fetch rows with index value strictly greater than this.
-    --output-format=<output_format>     Output rows in "dbx" (Splunk DBX) or "json" format [default: "dbx"].
+    --output-format=<output_format>     Output rows in "dbx" (Splunk DBX) or "json" format [default: dbx].
 """
 from docopt import docopt
 
 
+def list_tables(connection_string):
+    pass
+
+
+def run_query(connection_string, sql_query, index=None, offset=0, output_format='dbx'):
+    pass
+
+
 def _main():
     arguments = docopt(__doc__, version='datasnake 0.1.0')
+
+    if arguments['list-tables']:
+        list_tables(arguments['connection_string'])
+    else:
+        run_query(arguments['connection_string'], arguments['sql_query'], index=arguments['index'],
+                  offset=arguments['offset'], output_format=arguments['output_format'])
 
 
 if __name__ == '__main__':
