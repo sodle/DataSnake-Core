11e58cf0f59760b72a878f7fff4b0899bc319a5f
Scott Odle <sjodle@asu.edu>
2018-06-13 09:39:58 -0700

    version++

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='datasnake',
-    version='0.1.2',
+    version='0.1.3',
     description='CLI for fast and easy database queries, tailing, pagination, introspection. '
                 'DB engine agnostic. Backbone of Splunk TA-DataSnake.',
     author='Scott Odle (Arizona State University)',
